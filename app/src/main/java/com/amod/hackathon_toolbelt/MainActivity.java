f03593594be9436ef3ee1974631291b24363a7d2
amodnija <amodnij123@gmail.com>
2021-03-27 00:10:07 +0530

    calculator commit final

--- app/src/main/java/com/amod/hackathon_toolbelt/MainActivity.java
+++ app/src/main/java/com/amod/hackathon_toolbelt/MainActivity.java
@@ -6,10 +6,6 @@ import com.fathzer.soft.javaluator.DoubleEvaluator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-
-public class MainActivity extends AppCompatActivity {
-private Button b0;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
@@ -31,19 +27,6 @@ public class MainActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
-        b0 = (Button)findViewById(R.id.b0);
-
-        b0.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openConverter();
-            }
-        });
-    }
-    public void openConverter() {
-        Intent intent = new Intent(this, Converter.class);
-        startActivity(intent);
-
 
         button0 = (Button) findViewById(R.id.button0);
         button1 = (Button) findViewById(R.id.button1);
