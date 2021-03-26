a07ebfcc9d251f442701ec191862b522cff0a910
VectorBS-07 <f20202008@goa.bits-pilani.ac.in>
2021-03-26 20:51:53 +0530

    bhavya somani-- converter

--- app/src/main/java/com/amod/hackathon_toolbelt/MainActivity.java
+++ app/src/main/java/com/amod/hackathon_toolbelt/MainActivity.java
@@ -2,13 +2,28 @@ package com.amod.hackathon_toolbelt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+private Button b0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b0 = (Button)findViewById(R.id.b0);
+
+        b0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openConverter();
+            }
+        });
+    }
+    public void openConverter() {
+        Intent intent = new Intent(this, Converter.class);
+        startActivity(intent);
     }
 }
